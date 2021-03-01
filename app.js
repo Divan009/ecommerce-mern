6aa6a49970b56a344133eae75c0ce39519ef8b5d
diva dugar <divadugar@gmail.com>
2021-03-01 20:06:30 +0530

    Added MongoDB to the app

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+// import mongoose
+const mongoose = require('mongoose');
+// load env variables
+const dotenv = require('dotenv');
+const express = require('express');
+require('dotenv').config();
 
+const app = express()
+
+dotenv.config()
+
+//db connection
+mongoose.connect(
+    process.env.DATABASE,
+    {useNewUrlParser: true}
+).then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+});
+
+// Route
 app.get("/", (req, res) => {
     res.send("hello from node nodemon");
 });
